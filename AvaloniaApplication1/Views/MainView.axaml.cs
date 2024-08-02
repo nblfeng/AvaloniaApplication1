7fa47d9435498febb48c2d8f456ed92fb47aeff5
nblfeng <nblfeng@qq.com>
2024-08-02 16:13:59 +0800

    doc

--- AvaloniaApplication1/Views/MainView.axaml.cs
+++ AvaloniaApplication1/Views/MainView.axaml.cs
@@ -12,28 +12,16 @@ public partial class MainView : UserControl
     public MainView( )
     {
         InitializeComponent( );
-
-      
-
     }
 
     protected override void OnLoaded( RoutedEventArgs e )
     {
         base.OnLoaded( e );
 
+        // android中闪退, 无法捕获异常
         if ( App.ServiceProvider != null )
         {
             var view = App.ServiceProvider.GetRequiredService<TestView>( );
-        }
-
-        {
-            var insetsManager = TopLevel.GetTopLevel( this )?.InsetsManager;
-
-            if ( insetsManager != null )
-            {
-                insetsManager.IsSystemBarVisible = false;
-                insetsManager.DisplayEdgeToEdge = true;
-            }
         }
     }
 }
